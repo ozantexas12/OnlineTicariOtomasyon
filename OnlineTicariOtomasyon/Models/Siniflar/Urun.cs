dd4052b6b5dcfed572ce590418321e87d401e46c
ozan <ozanpaker72@gmail.com>
2023-11-14 00:16:25 +0300

    ilk kayıt

---
+++ OnlineTicariOtomasyon/Models/Siniflar/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Urun
+    {
+        [Key]
+        public int UrunId { get; set; }
+        public string UrunAdı { get; set;}
+        public string UrunMarka {  get; set;}
+        public short UrunStok {  get; set;}
+        public decimal AlisFiyat { get; set;}
+        public decimal SatisFiyat { get; set;}
+        public bool UrunDurum {  get; set;}
+        public string UrunGorsel {  get; set;}
+    }
+}
