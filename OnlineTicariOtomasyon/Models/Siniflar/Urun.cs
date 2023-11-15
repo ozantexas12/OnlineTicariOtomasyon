84711c31e5dd851fbcc85244616e8102f5e51f10
ozan <ozanpaker72@gmail.com>
2023-11-15 16:18:30 +0300

    Ürün sınıfının tasarımı yapıldı

--- OnlineTicariOtomasyon/Models/Siniflar/Urun.cs
+++ OnlineTicariOtomasyon/Models/Siniflar/Urun.cs
@@ -11,21 +11,22 @@ namespace OnlineTicariOtomasyon.Models.Siniflar
     {
         [Key]
         public int UrunId { get; set; }
-        
-        [Column(TypeName ="Varchar")]
+
+        [Column(TypeName = "Varchar")]
         [StringLength(30)]
-        public string UrunAdı { get; set;}
-        public string UrunMarka {  get; set;}
-        public short UrunStok {  get; set;}
-        public decimal AlisFiyat { get; set;}
-        public decimal SatisFiyat { get; set;}
-        public bool UrunDurum {  get; set;}
+        public string UrunAdı { get; set; }
+        public string UrunMarka { get; set; }
+        public short UrunStok { get; set; }
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool UrunDurum { get; set; }
 
 
         [Column(TypeName = "Varchar")]
         [StringLength(250)]
-        public string UrunGorsel {  get; set;}
-        public Kategori Kategori { get; set;}
+        public string UrunGorsel { get; set; }
+        public int KategoriId {  get; set; }
+        public virtual Kategori Kategori { get; set; }
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
 }
