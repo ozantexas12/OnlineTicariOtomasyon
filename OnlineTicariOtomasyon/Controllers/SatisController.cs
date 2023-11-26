15eb5c6ec98e0e7604fa95aef7c378d96e7ca606
ozan <ozanpaker72@gmail.com>
2023-11-27 00:57:30 +0300

    Personellerin hangi Cariye ne Sattığını görebileceğimiz Satışlar Sayfası eklendi ve yapılan satışları sisteme girmek için sayfa Satışlar sayfasının içine eklendi.

---
+++ OnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Siniflar;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.SatisHarekets.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> deger1 = (from x in c.Uruns.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.UrunAdı,
+                                               Value = x.UrunId.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger2 = (from x in c.Carilers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CarilerAdı + "" + x.CarilerSoyadı,
+                                               Value = x.CarilerID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger3 = (from x in c.Personels.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PersonelAdı + "" + x.PersonelSoyadı,
+                                               Value = x.PersonelID.ToString()
+                                           }).ToList();
+            ViewBag.dgr3 = deger3;
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(SatisHareket s)
+        {
+            s.Tarih=DateTime.Parse(DateTime.Now.ToShortDateString());
+            c.SatisHarekets.Add(s);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
