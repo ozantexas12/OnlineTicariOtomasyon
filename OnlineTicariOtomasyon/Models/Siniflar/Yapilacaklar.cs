82fb6a699eda70a5969c0511e2f84f0f5a9bb1ee
ozan <ozanpaker72@gmail.com>
2023-12-09 19:19:58 +0300

    Sayfaya Yapılacaklar Listesi Eklendi.

---
+++ OnlineTicariOtomasyon/Models/Siniflar/Yapilacaklar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Yapilacaklar
+    {
+        [Key]
+        public int YapılacakİD { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(100)]
+        public string Baslik { get; set; }
+
+        [Column(TypeName = "bit")]
+        public bool Durum { get; set; }
+    }
+}
