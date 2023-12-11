1cca081047550924c0f56a371fd0bad9615088f5
ozan <ozanpaker72@gmail.com>
2023-12-11 12:37:18 +0300

    Düzenlemeler Yapıldı.

--- OnlineTicariOtomasyon/Models/Siniflar/Admin.cs
+++ OnlineTicariOtomasyon/Models/Siniflar/Admin.cs
@@ -10,18 +10,19 @@ namespace OnlineTicariOtomasyon.Models.Siniflar
     public class Admin
     {
         [Key]
-        public int AdminID {  get; set; }
+        public int AdminID { get; set; }
 
-        [Column(TypeName ="Varchar")]
+        [Column(TypeName = "Varchar")]
         [StringLength(10)]
-        public string KullanıcıAdi { get; set; }
+        public string KullaniciAdi { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
-        public string Sifre {  get; set; }
+        public string Sifre { get; set; }
 
-        [Column(TypeName ="Char")]
-        [StringLength (1)]
-        public string Yetki {  get; set; }
+        [Column(TypeName = "Char")]
+        [StringLength(1)]
+        public string Yetki { get; set; }
+
     }
 }
