8514855ce07399cb2c09c41873b7b39ff1c43515
ozan <ozanpaker72@gmail.com>
2023-12-18 22:24:32 +0300

    Cari Panelde Siparişlerim Kısmına Carinin Siparişleri Çekilemedi.

---
+++ OnlineTicariOtomasyon/Models/Siniflar/SinifGroup3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class SinifGroup3
+    {
+        public string Kategori {  get; set; }
+        public int Sayi { get; set; }
+    }
+}
