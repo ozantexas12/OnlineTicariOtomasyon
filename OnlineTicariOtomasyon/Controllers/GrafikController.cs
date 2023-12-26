e53cf439c4c118e9a6691c03db52f861f7fde6cc
ozan <ozanpaker72@gmail.com>
2023-12-26 19:00:24 +0300

    Grafik Sayfası eklendi Türlerine göre Sayfalar eklendi.

---
+++ OnlineTicariOtomasyon/Controllers/GrafikController.cs
@@ -0,0 +1,84 @@
+﻿using OnlineTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class GrafikController : Controller
+    {
+        // GET: Grafik
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult VisualizeUrunResult()
+        {
+            return Json(Urunlistesi(), JsonRequestBehavior.AllowGet);
+        }
+        public List<sinif1> Urunlistesi()
+        {
+            List<sinif1> snf = new List<sinif1>();
+            snf.Add(new sinif1()
+            {
+                urunad = "Blgisayar",
+                stoksayisi = 120
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Beyaz Eşya",
+                stoksayisi = 150
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Küçük Ev Aletleri",
+                stoksayisi = 180
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Mobil Cihazlar",
+                stoksayisi = 90
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Mobilya",
+                stoksayisi = 70
+            });
+            return snf;
+
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult VisualizeUrunResult2()
+        {
+            return Json(UrunListesi2(), JsonRequestBehavior.AllowGet);
+        }
+        public List<sinif2> UrunListesi2()
+        {
+            List<sinif2> snf = new List<sinif2>();
+            using (var c = new Context())
+            {
+                snf = c.Uruns.Select(x => new sinif2
+                {
+                       urn=x.UrunAdi,
+                       stk=x.UrunStok
+                }).ToList();
+            }
+            return snf;
+        }
+        public ActionResult Index2() 
+        { 
+            return View(); 
+        }
+        public ActionResult Index3() 
+        { 
+            return View(); 
+        }
+    }
+}
---
+++ OnlineTicariOtomasyon/Models/Siniflar/sinif1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineTicariOtomasyon.Models.Siniflar
+{
+    public class sinif1
+    {
+        public string urunad {  get; set; }
+        public int stoksayisi { get; set; }
+
+    }
+}
