e76729b364c915d48ac64c02b2c5876857784244
ozan <ozanpaker72@gmail.com>
2023-12-27 22:59:42 +0300

    Hata Eklendi.

---
+++ OnlineTicariOtomasyon/Controllers/HataSayfalarıController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class HataSayfalarıController : Controller
+    {
+        // GET: HataSayfaları
+        public ActionResult PageError()
+        {
+            Response.TrySkipIisCustomErrors=true;
+
+            return View();
+        }
+        public ActionResult Page404()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View("PageError");  
+
+        }
+    }
+}
